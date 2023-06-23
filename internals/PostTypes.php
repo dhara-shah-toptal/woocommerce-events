cc772388cf7300f821f29882eac9e5e3a12e11f5
Dhara Shah <dhara.shah@toptal.com>
2023-06-23 15:00:58 +0530

    Custom Post Type - Event

--- internals/PostTypes.php
+++ internals/PostTypes.php
@@ -28,6 +28,10 @@ class PostTypes extends Base {
 		parent::initialize();
 
 		\add_action( 'init', array( $this, 'load_cpts' ) );
+		\add_action( 'add_meta_boxes', array( $this, 'add_custom_meta_box_event_details' ), 10, 2 );
+		\add_action( 'post_edit_form_tag', array( $this, 'update_event_details_form' ));
+		\add_action( 'save_post',array( $this, 'save_custom_meta_box_event_details' ));
+
 		// Add bubble notification for cpt pending
 		\add_action( 'admin_menu', array( $this, 'pending_cpt_bubble' ), 999 );
 		\add_filter( 'pre_get_posts', array( $this, 'filter_search' ) );
@@ -61,85 +65,176 @@ class PostTypes extends Base {
 	 */
 	public function load_cpts() { //phpcs:ignore
 		// Create Custom Post Type https://github.com/johnbillion/extended-cpts/wiki
-		$demo_cpt = \register_extended_post_type(
-				'demo',
-				array(
-					// Show all posts on the post type archive:
-					'archive'            => array(
-						'nopaging' => true,
-					),
-					'slug'               => 'demo',
-					'show_in_rest'       => true,
-					'dashboard_activity' => true,
-					'capability_type'    => array( 'demo', 'demoes' ),
-					// Add some custom columns to the admin screen
-					'admin_cols'         => array(
-						'featured_image' => array(
-							'title'          => 'Featured Image',
-							'featured_image' => 'thumbnail',
-						),
-						'title',
-						'genre'          => array(
-							'taxonomy' => 'demo-section',
-						),
-						'custom_field'   => array(
-							'title'    => 'By Lib',
-							'meta_key' => '_demo_' . W_TEXTDOMAIN . '_text', // phpcs:ignore
-							'cap'      => 'manage_options',
-						),
-						'date'           => array(
-							'title'   => 'Date',
-							'default' => 'ASC',
-						),
-					),
-					// Add a dropdown filter to the admin screen:
-					'admin_filters'      => array(
-						'genre' => array(
-							'taxonomy' => 'demo-section',
-						),
-					),
+		$events_cpt = \register_extended_post_type( 'event', array(
+			'labels'             => array(
+				'name'                  => _x( 'Events', 'Events', W_TEXTDOMAIN ),
+				'singular_name'         => _x( 'Event', 'Event', W_TEXTDOMAIN ),
+				'menu_name'             => _x( 'Events', 'Events', W_TEXTDOMAIN ),
+				'name_admin_bar'        => _x( 'Event', 'Add New Event', W_TEXTDOMAIN ),
+				'add_new'               => __( 'Add New', W_TEXTDOMAIN ),
+				'add_new_item'          => __( 'Add New Event', W_TEXTDOMAIN ),
+				'new_item'              => __( 'New Event', W_TEXTDOMAIN ),
+				'edit_item'             => __( 'Edit Event', W_TEXTDOMAIN ),
+				'view_item'             => __( 'View Event', W_TEXTDOMAIN ),
+				'all_items'             => __( 'All Events', W_TEXTDOMAIN ),
+				'search_items'          => __( 'Search Events', W_TEXTDOMAIN ),
+				'parent_item_colon'     => __( 'Parent Events:', W_TEXTDOMAIN ),
+				'not_found'             => __( 'No Events found.', W_TEXTDOMAIN ),
+				'not_found_in_trash'    => __( 'No Events found in Trash.', W_TEXTDOMAIN ),
+				'featured_image'        => _x( 'Event Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', W_TEXTDOMAIN ),
+				'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', W_TEXTDOMAIN ),
+				'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', W_TEXTDOMAIN ),
+				'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', W_TEXTDOMAIN ),
+				'archives'              => _x( 'Event archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', W_TEXTDOMAIN ),
+				'insert_into_item'      => _x( 'Insert into Event', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', W_TEXTDOMAIN ),
+				'uploaded_to_this_item' => _x( 'Uploaded to this Event', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', W_TEXTDOMAIN ),
+				'filter_items_list'     => _x( 'Filter Events list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', W_TEXTDOMAIN ),
+				'items_list_navigation' => _x( 'Events list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', W_TEXTDOMAIN ),
+				'items_list'            => _x( 'Events list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', W_TEXTDOMAIN ),
 			),
-			array(
-				// Override the base names used for labels:
-				'singular' => \__( 'Demo', W_TEXTDOMAIN ),
-				'plural'   => \__( 'Demos', W_TEXTDOMAIN ),
-			)
-		);
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'Event' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+		) );
 
-		$demo_cpt->add_taxonomy( 'demo-section', array( 'hierarchical' => false, 'show_ui' => false ) );
-		// Create Custom Taxonomy https://github.com/johnbillion/extended-taxos
-		\register_extended_taxonomy(
-			'demo-section',
-			'demo',
-			array(
-				// Use radio buttons in the meta box for this taxonomy on the post editing screen:
-				'meta_box'         => 'radio',
-				// Show this taxonomy in the 'At a Glance' dashboard widget:
-				'dashboard_glance' => true,
-				// Add a custom column to the admin screen:
-				'admin_cols'       => array(
-					'featured_image' => array(
-						'title'          => 'Featured Image',
-						'featured_image' => 'thumbnail',
-					),
-				),
-				'slug'             => 'demo-cat',
-				'show_in_rest'     => true,
-				'capabilities'     => array(
-					'manage_terms' => 'manage_demoes',
-					'edit_terms'   => 'manage_demoes',
-					'delete_terms' => 'manage_demoes',
-					'assign_terms' => 'read_demo',
-				),
-			),
-			array(
-				// Override the base names used for labels:
-				'singular' => \__( 'Demo Category', W_TEXTDOMAIN ),
-				'plural'   => \__( 'Demo Categories', W_TEXTDOMAIN ),
-			)
+
+	}
+
+	function add_custom_meta_box_event_details( $post_type, $post ) {
+		\add_meta_box(
+			'event-details',
+			__( 'Event Details' ),
+			array( $this, 'render_custom_meta_box_event_details' ),
+			'event',
+			'normal',
+			'default'
 		);
 	}
 
+	function render_custom_meta_box_event_details( $post ) {
+
+		// Add an nonce field so we can check for it later.
+		wp_nonce_field( 'event_custom_box', 'event_custom_box_nonce' );
+
+		// Use get_post_meta to retrieve an existing value from the database.
+		$we_event_name = \get_post_meta( $post->ID, '_we_event_name', true );
+		$we_event_date = \get_post_meta( $post->ID, '_we_event_date', true );
+		$we_event_city = \get_post_meta( $post->ID, '_we_event_city', true );
+		$we_event_description = \get_post_meta( $post->ID, '_we_event_description', true );
+		$we_event_image = \get_post_meta( $post->ID, '_we_event_image', true );
+
+
+		// Display the form, using the current value.
+		?>
+		<div style="padding:10px;">
+		<label for="we_event_name" style="display:inline-block; width:100px;vertical-align:top;">
+			<?php _e( 'Event Name', W_TEXTDOMAIN ); ?>
+		</label>
+		<input type="text" id="we_event_name" name="we_event_name" value="<?php echo esc_attr( $we_event_name ); ?>" size="50" />
+		</div>
+		<div style="padding:10px;">
+		<label for="we_event_date" style="display:inline-block; width:100px;vertical-align:top;">
+			<?php _e( 'Event Date', W_TEXTDOMAIN ); ?>
+		</label>
+		<input type="date" id="we_event_date" name="we_event_date" value="<?php echo esc_attr( $we_event_date ); ?>" size="50" />
+		</div>
+		<div style="padding:10px;">
+		<label for="we_event_city" style="display:inline-block; width:100px;vertical-align:top;">
+			<?php _e( 'Event City', W_TEXTDOMAIN ); ?>
+		</label>
+		<input type="text" id="we_event_city" name="we_event_city" value="<?php echo esc_attr( $we_event_city ); ?>" size="50" />
+		</div>
+
+		<div style="padding:10px;">
+		<label for="we_event_description" style="display:inline-block; width:100px;vertical-align:top;">
+			<?php _e( 'Event Description', W_TEXTDOMAIN ); ?>
+		</label>
+		<textarea id="we_event_description" name="we_event_description" rows="5" cols="100"><?php echo esc_attr( $we_event_description ); ?> </textarea>
+		</div>
+
+		<div style="padding:10px;">
+		<label for="we_event_image" style="display:inline-block; width:100px;vertical-align:top;">
+			<?php _e( 'Event Image', W_TEXTDOMAIN ); ?>
+		</label>
+		<input type="file" id="we_event_image" name="we_event_image" value="" size="50" />
+		<img src="<?php echo $we_event_image['url']?>" height="50" />
+		</div>
+		<?php
+	}
+
+	function update_event_details_form($post) {
+		if($post->post_type === 'event'){
+			echo ' enctype="multipart/form-data"';
+		}
+	} // end update_edit_form
+
+	function save_custom_meta_box_event_details($post_id){
+		global $post;
+		if ($post->post_type != 'event'){
+			return;
+		}
+		// Check if our nonce is set.
+		if ( ! isset( $_POST['event_custom_box_nonce'] ) ) {
+			return $post_id;
+		}
+
+		$nonce = $_POST['event_custom_box_nonce'];
+
+		// Verify that the nonce is valid.
+		if ( ! wp_verify_nonce( $nonce, 'event_custom_box' ) ) {
+			return $post_id;
+		}
+
+		/*
+		 * If this is an autosave, our form has not been submitted,
+		 * so we don't want to do anything.
+		 */
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return $post_id;
+		}
+
+		// Check the user's permissions.
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+				return $post_id;
+		}
+
+
+		$we_event_name = sanitize_text_field( $_POST['we_event_name'] );
+		update_post_meta( $post_id, '_we_event_name', $we_event_name );
+
+		$we_event_date = sanitize_text_field( $_POST['we_event_date'] );
+		update_post_meta( $post_id, '_we_event_date', $we_event_date );
+
+		$we_event_city = sanitize_text_field( $_POST['we_event_city'] );
+		update_post_meta( $post_id, '_we_event_city', $we_event_city );
+
+		$we_event_description = sanitize_text_field( $_POST['we_event_description'] );
+		update_post_meta( $post_id, '_we_event_description', $we_event_description );
+
+
+
+		// Make sure the file array isn't empty
+		if(!empty($_FILES['we_event_image']['name'])) {
+
+			// Use the WordPress API to upload the file
+			$upload = wp_upload_bits($_FILES['we_event_image']['name'], null, file_get_contents($_FILES['we_event_image']['tmp_name']));
+
+			if(isset($upload['error']) && $upload['error'] != 0) {
+				wp_die('There was an error uploading your file. The error is: ' . $upload['error']);
+			} else {
+				add_post_meta($post_id, '_we_event_image', $upload);
+				update_post_meta($post_id, '_we_event_image', $upload);
+			} // end if/else
+		} // end if
+	}
 	/**
 	 * Bubble Notification for pending cpt<br>
 	 * NOTE: add in $post_types your cpts<br>
